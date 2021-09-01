6b5a88595519401daa06750d2f15dd6535d7a929
Caio-de-Paula-Brito <caiopbrito2005@gmail.com>
2021-09-01 17:24:22 -0300

    :tada:/creating-base-and-organization

---
+++ src/pages/FrontPage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as Styled from "./styles"
+
+const FrontPage: React.FC = ()=>{
+    return(
+        <Styled.Main>
+            <h1>
+                Bem vindo
+            </h1>
+        </Styled.Main>
+    )
+}
+
+export default FrontPage;
